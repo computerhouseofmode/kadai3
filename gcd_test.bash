7d5a44f5ab300dcaf549e9fcd19d2b5531f1abb5
computerhouseofmode <computerhouseofmode@users.noreply.github.com>
2015-06-13 01:12:21 +0900

    add test file

---
+++ gcd_test.bash
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+aa=$1
+bb=$2
+cc=$3
+#echo ${aa}
+#echo ${bb}
+#echo ${cc}
+
+dd=`./15746137 ${aa} ${bb}`
+#echo ${dd}
+
+if [ ${dd} -eq ${cc} ] ;then
+ echo "OK";exit 0
+else
+ echo "NG";exit 1
+fi
+
